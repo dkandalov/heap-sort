fcd179891b5290380be0d5a16cbb948f7cfde103
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-01-25 11:08:28 +0000

    misc

--- src/HeapSortTests.kt
+++ src/HeapSortTests.kt
@@ -1,99 +1,88 @@
-import kotlincommon.listOfInts
 import kotlincommon.permutations
 import kotlincommon.printed
 import kotlincommon.swap
 import kotlincommon.test.shouldEqual
 import org.junit.Test
-import kotlin.random.Random
+import java.util.*
 
 class HeapSortTests {
     @Test fun `sort list of integers`() {
-        emptyList<Int>().heapSort() shouldEqual emptyList()
+        fun List<Int>.canBeSorted() =
+            this.permutations().forEach {
+                it.printed().heapSort() shouldEqual this
+            }
 
-        listOf(1, 2, 3).permutations().forEach {
-            it.printed().heapSort() shouldEqual listOf(1, 2, 3)
-        }
-
-        listOf(1, 2, 3, 4).permutations().forEach {
-            it.printed().heapSort() shouldEqual listOf(1, 2, 3, 4)
-        }
-        listOf(1, 2, 3, 4, 5).permutations().forEach {
-            it.printed().heapSort() shouldEqual listOf(1, 2, 3, 4, 5)
-        }
-
-        val list = Random.listOfInts(
-            sizeRange = 0..100,
-            valuesRange = 0..100
-        )
-        list.heapSort().windowed(size = 2)
-            .all { it[0] <= it[1] } shouldEqual true
+        emptyList<Int>().canBeSorted()
+        listOf(1, 2, 3).canBeSorted()
+        listOf(1, 2, 3, 4).canBeSorted()
+        listOf(1, 2, 3, 4, 5).canBeSorted()
+        listOf(1, 2, 3, 4, 5, 6).canBeSorted()
     }
 
-    @Test fun `array-backed binary heap`() {
+    @Test fun `array-based binary heap`() {
         val heap = BinaryHeap()
+
         heap.add(4)
-        heap.add(1)
-        heap.add(2)
         heap.add(3)
+        heap.add(2)
+        heap.add(1)
 
         heap.remove() shouldEqual 1
-        heap.size shouldEqual 3
-
         heap.remove() shouldEqual 2
-        heap.size shouldEqual 2
-
         heap.remove() shouldEqual 3
-        heap.size shouldEqual 1
-
         heap.remove() shouldEqual 4
-        heap.size shouldEqual 0
     }
 }
 
-fun List<Int>.heapSort(): List<Int> = sequence {
+fun List<Int>.heapSort(): List<Int> {
     val heap = BinaryHeap()
     forEach { heap.add(it) }
-    while (heap.size > 0) {
-        yield(heap.remove())
+
+    val result = ArrayList<Int>()
+    while (heap.isNotEmpty()) {
+        result.add(heap.remove())
     }
-}.toList()
+    return result
+}
 
 class BinaryHeap {
     private val array = Array(100, { 0 })
-    var size = 0
+    private var size = 0
 
     fun add(element: Int) {
         array[size++] = element
-        siftUp(size - 1)
+        pullUp(size - 1)
     }
 
     fun remove(): Int {
         val result = array[0]
         array[0] = array[size - 1]
         size--
-        siftDown(0)
+        pushDown(0)
         return result
     }
 
-    private fun siftUp(index: Int) {
-        if (index == 0) return
-        val parentIndex = (index - 1) / 2
-        if (array[parentIndex] > array[index]) {
-            array.swap(parentIndex, index)
-            siftUp(parentIndex)
-        }
-    }
-
-    private fun siftDown(index: Int) {
-        val leftChild = index * 2 + 1
-        val rightChild = index * 2 + 2
-        val minIndex = listOf(index, leftChild, rightChild)
+    private fun pushDown(index: Int) {
+        val minIndex = listOf(index, index * 2 + 1, index * 2 + 2)
             .filter { it < size }
             .minBy { array[it] } ?: return
 
         if (minIndex != index) {
             array.swap(minIndex, index)
-            siftDown(minIndex)
+            pushDown(minIndex)
         }
+    }
+
+    private fun pullUp(index: Int) {
+        if (index == 0) return
+        val parentIndex = (index - 1) / 2
+        if (array[parentIndex] > array[index]) {
+            array.swap(parentIndex, index)
+            pullUp(parentIndex)
+        }
+    }
+
+    fun isNotEmpty(): Boolean {
+        return size > 0
     }
 }
